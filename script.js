529149e6e8b06d5c4d6fa175dc41afffa35ea902
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-23 22:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const priceTable = document.getElementsByTagName('table')[0];
+const prices = document.querySelectorAll(".price");
+
 const getSum = () => {
-//Add your code here
-  
+	let total = 0;
+	prices.forEach((price)=>{
+		total += parseInt(price.innerText);
+	})
+	
+	const td = document.createElement('td');
+    td.innerText = total;
+	priceTable.appendChild(td);
 };
 
 getSumBtn.addEventListener("click", getSum);
